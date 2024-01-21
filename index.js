a79381d94d0e2a687b2465ade6c6661f25fac4f9
Ilya Lobanovskiy <156040021+vadimov1ch@users.noreply.github.com>
2024-01-21 23:01:42 +0300

    feat: add pathes

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ import mongoose from 'mongoose';
 import multer from 'multer';
 import cors from 'cors';
 
-// import {
-    
-// } from './controllers/index.js';
+import {
+    userController,
+    employeeController,
+    machineController,
+    orderController,
+} from './controllers/index.js';
 
-// import {
-    
-// } from './validations.js';
+import {
+    loginValidation,
+    registerValidation,
+    createEmployeeValidate,
+    updateEmployeeValidate,
+    createMachineValidation,
+    updateMachineValidation,
+    createOrderValidation,
+    updateOrderValidation,
+} from './validations.js';
 
-// import {
-    
-// } from './utils/index.js';
+import {
+    handleValidationErrors,
+    allRolesAuth,
+    adminOnlyAuth,
+} from './utils/index.js';
 
 mongoose
     .connect('mongodb+srv://admin:He12345678@cluster0.k6wg7rw.mongodb.net/Laundry?retryWrites=true&w=majority')
@@ -21,6 +33,108 @@ mongoose
     .catch((err) => console.log('DB ERROR', err));
 
 const app = express();
+
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+// media upload pathes
+app.post('/upload', adminOnlyAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
+
+//auth
+app.post('/auth/login', loginValidation, handleValidationErrors, userController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, userController.register);
+app.get('/auth/me', allRolesAuth, userController.getMe);
+
+//employees
+app.post('/employees/create',
+    adminOnlyAuth,
+    createEmployeeValidate,
+    handleValidationErrors,
+    employeeController.create
+);
+app.delete('/employees/:id/delete',
+    adminOnlyAuth,
+    employeeController.remove
+);
+app.patch('/employees/:id/update',
+    adminOnlyAuth,
+    updateEmployeeValidate,
+    handleValidationErrors,
+    employeeController.update
+);
+app.get('/employees/:id',
+    employeeController.getOne
+);
+app.get('/employees',
+    employeeController.getAll
+);
+
+//machine
+app.post('/machine/create',
+    adminOnlyAuth,
+    createMachineValidation,
+    handleValidationErrors,
+    machineController.create
+);
+app.delete('/machine/:id/delete',
+    adminOnlyAuth,
+    machineController.remove
+);
+app.patch('/machine/:id/update',
+    adminOnlyAuth,
+    updateMachineValidation,
+    handleValidationErrors,
+    machineController.update
+);
+app.get('/machine/:id',
+    machineController.getOne
+);
+app.get('/machine',
+    machineController.getAll
+);
+
+//orders
+app.post('/orders/create',
+    allRolesAuth,
+    createOrderValidation,
+    handleValidationErrors,
+    orderController.create
+);
+app.delete('/orders/:id/delete',
+    adminOnlyAuth,
+    orderController.remove
+);
+app.patch('/orders/:id/update',
+    adminOnlyAuth,
+    updateOrderValidation,
+    handleValidationErrors,
+    orderController.update
+);
+app.get('/orders/:id',
+    allRolesAuth,
+    orderController.getOne
+);
+app.get('/orders',
+    allRolesAuth,
+    orderController.getAll
+);
 
 app.listen(4444, (err) => {
     if (err) {
